16ca5c082a887e70024573c5a62be458478ae388
Dawid Piechaczek <dawidpiechaczek.20@gmail.com>
2021-03-11 16:09:14 +0100

    Gallery layout added.

--- lib/gallery.dart
+++ lib/gallery.dart
@@ -12,26 +12,89 @@ class GalleryPage extends StatefulWidget {
 class _GalleryPageState extends State<GalleryPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: homeBackground(),
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: null,
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[Text('data')],
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: galleryAppBar(),
+      body: Center(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              addPhotoTitle(),
+              addPhotoMessage(),
+              photosContainer(),
+              skipButton()
+            ]),
+      ),
+    );
+  }
+
+  Expanded photosContainer() {
+    return Expanded(
+      child: GridView.count(
+        padding: EdgeInsets.only(right: 8, left: 8),
+        childAspectRatio: 3 / 4,
+        crossAxisCount: 3,
+        children: List.generate(9, (index) {
+          return Padding(
+            padding: EdgeInsets.all(4),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Color(0xFFE0E0E0),
+                  borderRadius: BorderRadius.circular(8)),
+              child: Center(
+                child: index == 0 ? Icon(Icons.add) : null,
+              ),
             ),
-          ),
+          );
+        }),
+      ),
+    );
+  }
+
+  FlatButton skipButton() {
+    return FlatButton(
+      onPressed: null,
+      child: Text(
+        "POMIŃ",
+        style: TextStyle(fontSize: 16),
+      ),
+    );
+  }
+
+  Padding addPhotoMessage() {
+    return Padding(
+        padding: EdgeInsets.only(top: 8, bottom: 16, right: 16, left: 16),
+        child: Text(
+          "Pierwsze zdjęcie w Twoim albumie musi przedstawiać Ciebie",
+          textAlign: TextAlign.center,
         ));
   }
 
-  BoxDecoration homeBackground() {
-    return BoxDecoration(
-        gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            stops: [0.3, 0.5, 0.9],
-            colors: [Color(0xFFB3E5FC), Color(0xFF81D4FA), Color(0xFF4FC3F7)]));
+  Padding addPhotoTitle() {
+    return Padding(
+      padding: EdgeInsets.only(top: 16, right: 16, left: 16),
+      child: Text(
+        "Przeciągnij i upuść, aby uporządkować swoje zdjęcia!",
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  AppBar galleryAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      iconTheme: IconThemeData(color: Colors.blue),
+      actionsIconTheme: IconThemeData(color: Colors.blue),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.check, color: Colors.blue),
+        )
+      ],
+      title: Text(
+        "Zdjęcia",
+        style: TextStyle(color: Colors.black),
+      ),
+    );
   }
 }
